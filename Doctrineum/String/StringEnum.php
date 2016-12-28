385a43bb2e94d476ead180fb191183915318f350
Jaroslav Týc <mail@jaroslavtyc.com>
2016-12-28 08:34:52 +0100

    Return value annotated

--- Doctrineum/String/StringEnum.php
+++ Doctrineum/String/StringEnum.php
@@ -5,12 +5,15 @@ use Doctrineum\Scalar\ScalarEnum;
 use Granam\Scalar\Tools\ToString;
 use Granam\String\StringInterface;
 
+/**
+ * @method static StringEnum getEnum($value)
+ */
 class StringEnum extends ScalarEnum implements StringInterface
 {
     /**
      * @param bool|float|int|string|object $enumValue
-     *
      * @return string
+     * @throws \Doctrineum\String\Exceptions\UnexpectedValueToEnum
      */
     protected static function convertToEnumFinalValue($enumValue)
     {
