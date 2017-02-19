caeac2ad1180c5c0c78c8ae821a0e3c7fa669c86
Jaroslav Týc <mail@jaroslavtyc.com>
2017-02-19 07:27:25 +0100

    PHP 7+ required

--- Doctrineum/Tests/String/StringEnumTest.php
+++ Doctrineum/Tests/String/StringEnumTest.php
@@ -3,8 +3,9 @@ namespace Doctrineum\Tests\String;
 
 use Doctrineum\String\StringEnum;
 use Granam\String\StringInterface;
+use PHPUnit\Framework\TestCase;
 
-class StringEnumTest extends \PHPUnit_Framework_TestCase
+class StringEnumTest extends TestCase
 {
     /**
      * @test
